94ff19bd2c4932618a5ed2ac5810fd02cfb9a579
geemili <opensource@geemili.xyz>
2024-10-31 16:01:47 -0600

    add `mat.transpose()`

--- src/geometry/mat.zig
+++ src/geometry/mat.zig
@@ -1,23 +1,43 @@
 //! Matrix math operations
 
 pub fn mul(comptime M: usize, comptime N: usize, comptime P: usize, comptime T: type, a: [N][M]T, b: [P][N]T) [P][M]T {
+    const a_t = transpose(N, M, T, a);
+
     var res: [P][M]T = undefined;
 
     for (&res, 0..) |*column, i| {
+        const vb: @Vector(N, T) = b[i];
         for (column, 0..) |*c, j| {
-            var va: @Vector(N, T) = undefined;
-            comptime var k: usize = 0;
-            inline while (k < N) : (k += 1) {
-                va[k] = a[k][j];
-            }
-
-            const vb: @Vector(N, T) = b[i];
+            const va: @Vector(N, T) = a_t[j];
 
             c.* = @reduce(.Add, va * vb);
         }
     }
 
     return res;
+}
+
+pub fn transpose(comptime N: usize, comptime M: usize, comptime T: type, matrix: [N][M]T) [M][N]T {
+    var result: [M][N]T = undefined;
+
+    for (0..M) |i| {
+        for (0..N) |j| {
+            result[i][j] = matrix[j][i];
+        }
+    }
+
+    return result;
+}
+
+test {
+    try std.testing.expectEqualDeep([2][3]f32{
+        .{ 7, 9, 11 },
+        .{ 8, 10, 12 },
+    }, transpose(3, 2, f32, .{
+        .{ 7, 8 },
+        .{ 9, 10 },
+        .{ 11, 12 },
+    }));
 }
 
 const std = @import("std");
